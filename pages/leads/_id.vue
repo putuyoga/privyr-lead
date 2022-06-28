7e8061988e97b75616675d9e88d1325a3fadec86
I Putu Yoga Permana <putuyoga@gmail.com>
2022-06-28 18:04:11 +0700

    initial commit

---
+++ pages/leads/_id.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="url">http://localhost/webhooks/YIAS43</div>
+    <div class="div-table">
+      <div class="div-table-row">
+        <div class="div-table-col" align="center">Name</div>
+        <div class="div-table-col">Email</div>
+        <div class="div-table-col">Phone</div>
+      </div>
+      <div v-for="lead in leads" :key="lead.name" class="div-table-row">
+        <div class="div-table-col">{{ lead.name }}</div>
+        <div class="div-table-col">{{ lead.email }}</div>
+        <div class="div-table-col">{{ lead.phone }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    leads: [
+      { name: 'Gunther', email: 'sato@gunther.com', phone: '+6212312321' },
+      { name: 'Mark', email: 'mark@gunther.com', phone: '+626571231' },
+      { name: 'Lodash', email: 'lodash@gunther.com', phone: '+101282321' },
+    ],
+  }),
+})
+</script>
+
+<style scoped>
+.url {
+  padding: 12px;
+}
+</style>
